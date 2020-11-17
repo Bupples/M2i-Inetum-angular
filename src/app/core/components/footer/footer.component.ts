cb2452a13b1251aecda3d486a0c3ae91ff536654
Arthur Gerey <arthur.gerey@gmail.com>
2020-11-17 12:38:55 +0100

    webpack bundle analyzer + compodoc

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  public date = dayjs().format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A') // display
   constructor() { }
 
   ngOnInit(): void {
